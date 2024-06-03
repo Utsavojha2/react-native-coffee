18fbce1a61e5c8aabfda4bbe05a71996e21e5f45
Utsavojha2 <utsavojha2@gmail.com>
2024-06-03 08:56:08 +0545

    tweak env ecommand used in eas build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,34 @@
 import { useFonts } from "expo-font";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Slot, SplashScreen } from "expo-router";
+import { AppState } from "react-native";
+import { supabase } from "../supabase";
+import { Session } from "@supabase/supabase-js";
+
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
 
 export default function Layout() {
   const [loaded, error] = useFonts({
     Sora: require("../assets/font/Sora.ttf"),
-    
   });
+
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
 
   useEffect(() => {
     if (error) throw error;
